bb477163c17c9c1819907bc680fdb4d5b0d01ad9
AnkitGole <aggole@wpi.edu>
2025-04-19 15:26:08 -0400

    Only adding few features and exporting data in CSV Format

--- test_phase/src/data_preprocessing/etl.py
+++ test_phase/src/data_preprocessing/etl.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
@@ -17,49 +18,62 @@ engine = create_engine(
     connect_args={"application_name": "ETL_Optimized"}
 )
 
-def create_sample_data(engine, rows=2000000):
-    print("⚙️ Sampling and saving 2M rows directly from PostgreSQL...")
+def create_sample_data(engine):
+    print("⚙️ Sampling and saving rows directly from PostgreSQL...")
     with engine.connect() as conn:
         conn.execute(text(f"""
-            CREATE TABLE IF NOT EXISTS yelp_sample_data AS
+            CREATE TABLE IF NOT EXISTS yelp_data AS
             SELECT *
             FROM merged_full_data
-            ORDER BY RANDOM()
-            LIMIT {rows};
+            ORDER BY RANDOM();
         """))
-    print("✅ Sampled data saved to 'yelp_sample_data'")
+    print("✅ Sampled data saved to 'yelp_data'")
 
 def data_processing():
     print("🔍 Loading sampled data into pandas DataFrame...")
-    df = pd.read_sql("SELECT * FROM yelp_sample_data", engine)
+    df = pd.read_sql("SELECT * FROM yelp_data", engine)
 
-    df["review_length"] = df["text"].fillna('').apply(lambda x: len(x.strip().split()))
-    df["yelping_since"] = pd.to_datetime(df["yelping_since"], errors="coerce")
-    df["yelping_since_days"] = (pd.Timestamp.today() - df["yelping_since"]).dt.days
+    # df["review_length"] = df["text"].fillna('').apply(lambda x: len(x.strip().split()))
+    # df["yelping_since"] = pd.to_datetime(df["yelping_since"], errors="coerce")
+    # df["yelping_since_days"] = (pd.Timestamp.today() - df["yelping_since"]).dt.days
 
-    df["elite"] = df["elite"].fillna('').astype(str)
-    df["elite_years"] = df["elite"].apply(lambda x: len(x.split(',')) if x.strip() else 0)
+    # df["elite"] = df["elite"].fillna('').astype(str)
+    # df["elite_years"] = df["elite"].apply(lambda x: len(x.split(',')) if x.strip() else 0)
 
-    df["user_friends"] = df["user_friends"].fillna('').astype(str)
-    df["friends_count"] = df["user_friends"].apply(lambda x: len(x.split(',')) if x.strip() else 0)
+    # df["user_friends"] = df["user_friends"].fillna('').astype(str)  
+    # df["friends_count"] = df["user_friends"].apply(lambda x: len(x.split(',')) if x.strip() else 0)
 
     df["review_stars"] = pd.to_numeric(df["review_stars"], errors="coerce")
     df["biz_stars"] = pd.to_numeric(df["biz_stars"], errors="coerce")
     df["rating_deviation"] = (df["review_stars"] - df["biz_stars"]).abs()
 
+    # df["user_age"] = np.random.randint(12, 71, size=len(df))
+
     print("💾 Saving processed sample data back to PostgreSQL...")
-    df.to_sql("yelp_sample_data", engine, if_exists="replace", index=False)
+    df.to_sql("yelp_data", engine, if_exists="replace", index=False)
     print("✅ Data processed and saved successfully.")
 
-def csv_data(file_path):
-    print("🔍 Loading processed sampled data into pandas DataFrame...")
-    df = pd.read_sql("SELECT * FROM yelp_sample_data", engine)
+def csv_data(engine):
+    with engine.begin() as conn:
+        print("🔍 Loading processed sampled data into pandas DataFrame...")
+        reviews_df = pd.read_sql("SELECT review_id, user_id, business_id, review_stars, text, date, rating_deviation FROM yelp_data LIMIT 20000", engine)
+        reviews_df.to_csv("test_phase/data/processed/reviews.csv", index=False, encoding="utf-8")
 
-    print("Saving DataFrame into CSV Format...")
-    df.to_csv(file_path + "yelp_sample_data.csv", index=False, encoding="utf-8")
+        # users_df = pd.read_sql("SELECT user_id, user_name, user_review_count, yelping_since, elite_years, friends_count, average_stars, user_age FROM yelp_data", engine)
+        # users_df.to_csv("test_phase/data/processed/users.csv", index=False, encoding="utf-8")
+
+        # business_df = pd.read_sql("SELECT business_id, biz_name, biz_city, biz_state, biz_stars, biz_review_count, categories FROM yelp_data", engine)
+        # business_df.to_csv("test_phase/data/processed/business.csv", index=False, encoding="utf-8")
+
+        users_df = pd.read_sql("SELECT user_id, user_name, user_review_count, yelping_since, elite, friends, average_stars, user_age FROM users", engine)
+        users_df.to_csv("test_phase/data/processed/users.csv", index=False, encoding="utf-8")
+
+        business_df = pd.read_sql("SELECT business_id, biz_name, biz_city, biz_state, biz_stars, biz_review_count, categories FROM business", engine)
+        business_df.to_csv("test_phase/data/processed/business.csv", index=False, encoding="utf-8")
+
     print("✅ Data saved in CSV Format successfully.")
 
 if __name__ == "__main__":
     create_sample_data(engine)
     data_processing()
-    csv_data(file_path="test_phase/data/processed/")
+    csv_data(engine)
